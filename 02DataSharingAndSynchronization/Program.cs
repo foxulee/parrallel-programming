073c042e304907674703f776f58a0baddea3f4d1
foxulee <foxulee@163.com>
2018-02-23 12:20:44 -0500

    Initial commit

---
+++ 02DataSharingAndSynchronization/Program.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _02DataSharingAndSynchronization
+{
+    class Program
+    {
+        //one is to use lock(){}
+        class BankAccount1
+        {
+            private object padlock = new object();
+            public int Balance { get; private set; }
+
+            public void Deposit(int amount)
+            {
+
+                lock (padlock)
+                {
+                    // += is really two operations
+                    // op1 is temp <- get_Balance() + amount
+                    // op2 is set_Balance(temp)
+                    // something can happen _between_ op1 and op2
+
+                    Balance += amount;
+                }
+            }
+
+            public void Withdraw(int amount)
+            {
+                lock (padlock)
+                {
+                    Balance -= amount;
+                }
+            }
+        }
+
+        //the second way is to use Interlocked, only useful for atomically changing low-level primitives
+        class BankAccount2
+        {
+            private int balance;
+
+            public int Balance
+            {
+                get { return balance; }
+                private set { balance = value; }
+            }
+
+            // interlocked class contains atomic operations on variables
+            // atomic = cannot be interrupted
+            public void Deposit(int amount)
+            {
+                Interlocked.Add(ref balance, amount);
+            }
+
+            public void Withdraw(int amount)
+            {
+                Interlocked.Add(ref balance, -amount);
+                //balance -= amount;
+            }
+        }
+
+        //the third way is to use more powerful tool, Mutex
+        class BankAccount3
+        {
+            public int Balance { get; private set; }
+
+            public BankAccount3(int balance)
+            {
+                Balance = balance;
+            }
+
+            public void Deposit(int amount)
+            {
+                Balance += amount;
+            }
+
+            public void Withdraw(int amount)
+            {
+                Balance -= amount;
+            }
+
+            public void Transfer(BankAccount3 where, int amount)
+            {
+                where.Balance += amount;
+                Balance -= amount;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var tasks0 = new List<Task>();
+            var ba0 = new BankAccount2();
+
+            for (int i = 0; i < 10; ++i)
+            {
+                tasks0.Add(Task.Factory.StartNew(() =>
+                {
+                    for (int j = 0; j < 1000; ++j)
+                        ba0.Deposit(100);
+                }));
+                tasks0.Add(Task.Factory.StartNew(() =>
+                {
+                    for (int j = 0; j < 1000; ++j)
+                        ba0.Withdraw(100);
+                }));
+            }
+
+            Task.WaitAll(tasks0.ToArray());
+
+            Console.WriteLine($"Final balance is {ba0.Balance}.");
+
+            // show interlocked methods here
+
+            // Interlocked.MemoryBarrier is a wrapper for Thread.MemoryBarrier
+            // only required on memory systems that have weak memory ordering (e.g., Itanium)
+            // prevents the CPU from reordering the instructions such that those before the barrier
+            // execute after those after
+
+            Console.WriteLine("All done here.");
+
+            Console.WriteLine();
+
+            #region Mutex
+            var tasks = new List<Task>();
+            var ba = new BankAccount3(0);
+            var ba2 = new BankAccount3(0);
+
+            // many synchro types deriving from WaitHandle
+            // Mutex = mutual exclusion
+
+            // two types of mutexes
+            // this is a _local_ mutex
+            Mutex mutex = new Mutex();
+            Mutex mutex2 = new Mutex();
+
+            for (int i = 0; i < 10; ++i)
+            {
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    for (int j = 0; j < 1000; ++j)
+                    {
+                        bool haveLock = mutex.WaitOne();
+                        try
+                        {
+                            ba.Deposit(1); // deposit 10000 overall
+                        }
+                        finally
+                        {
+                            if (haveLock) mutex.ReleaseMutex();
+                        }
+                    }
+                }));
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    for (int j = 0; j < 1000; ++j)
+                    {
+                        bool haveLock = mutex2.WaitOne();
+                        try
+                        {
+                            ba2.Deposit(1); // deposit 10000
+                        }
+                        finally
+                        {
+                            if (haveLock) mutex2.ReleaseMutex();
+                        }
+                    }
+                }));
+
+                // transfer needs to lock both accounts
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    for (int j = 0; j < 1000; j++)
+                    {
+                        bool haveLock = Mutex.WaitAll(new[] { mutex, mutex2 });
+                        try
+                        {
+                            ba.Transfer(ba2, 1); // transfer 10k from ba to ba2
+                        }
+                        finally
+                        {
+                            if (haveLock)
+                            {
+                                mutex.ReleaseMutex();
+                                mutex2.ReleaseMutex();
+                            }
+                        }
+                    }
+                }));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            Console.WriteLine($"Final balance is: ba={ba.Balance}, ba2={ba2.Balance}.");
+            #endregion
+        }
+    }
+}
+
+
---
+++ ParallelProgramming/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ParallelProgramming
+{
+    class Program
+    {
+        public static void Write1(char c)
+        {
+            var i = 1000;
+            while (i-- > 0)
+            {
+                Console.Write(c);
+            }
+        }
+        public static void Write2(object o)
+        {
+            var i = 1000;
+            while (i-- > 0)
+            {
+                Console.Write(o);
+            }
+        }
+
+        public static int TextLength(object o)
+        {
+            Console.WriteLine($"\nTask with id {Task.CurrentId} processing object {o}");
+            return o.ToString().Length;
+        }
+        static void Main(string[] args)
+        {
+            #region Creating and Starting Task
+            //In two ways
+
+            //Task.Factory.StartNew(() => Write1('.'));
+            //Task.Factory.StartNew(Write2, '1');
+
+            //var task1 = new Task(() => Write1('?'));
+            //task1.Start();
+            //var task2 = new Task(Write2, '?');
+            //task2.Start();
+
+            //string text1 = "test1";
+            //string text2 = "tests2";
+            //var task3 = Task<int>.Factory.StartNew(TextLength, text1);
+            //var task4 = new Task<int>(TextLength, text2);
+            //task4.Start();
+
+            //Console.WriteLine($"Length of '{text1}' is {task3.Result}" );
+            //Console.WriteLine($"Length of '{text2}' is {task4.Result}" ); 
+            #endregion
+
+            #region Canceling Task
+
+            #region Simple cancellation
+            //var cts = new CancellationTokenSource();
+            //var token = cts.Token;
+
+            ////callback, as soon as the task is canceled
+            //token.Register(() =>
+            //    {
+            //        Console.WriteLine("Cancellation has been requested.");
+            //    }
+            //);
+
+            //var t = new Task(() =>
+            //{
+            //    int i = 0;
+            //    while (true)
+            //    {
+            //        //if (token.IsCancellationRequested)
+            //        //    //break; //1. soft cancel
+            //        //    throw new OperationCanceledException(); //2. hard cancel
+            //        //3. combine if statement and throw
+            //        token.ThrowIfCancellationRequested();
+            //        Console.WriteLine($"{i++}\t");
+            //    }
+            //}, token);
+
+            //t.Start();
+
+            //Console.ReadLine();
+            //cts.Cancel(); 
+            #endregion
+
+            #region Composite cancellation
+
+            //var planned = new CancellationTokenSource();
+            //var preventative = new CancellationTokenSource();
+            //var emergency = new CancellationTokenSource();
+
+            //var paranoid = CancellationTokenSource.CreateLinkedTokenSource(planned.Token, preventative.Token, emergency.Token);
+            //var t = Task.Factory.StartNew(() =>
+            //{
+            //    int i = 0;
+            //    while (true)
+            //    {
+            //        paranoid.Token.ThrowIfCancellationRequested();
+            //        Console.WriteLine($"{i++}\t");
+            //        Thread.Sleep(300);
+            //    }
+            //});
+
+            //Console.ReadLine();
+            ////any one of three is canceled will trigger paranoid
+            //planned.Cancel();
+            ////preventative.Cancel();
+            ////emergency.Cancel();
+
+            #endregion
+
+            #endregion
+
+            #region Waiting for Time to Pass
+
+            //var cts = new CancellationTokenSource();
+            //var token = cts.Token;
+            //var t = new Task(() =>
+            //{
+            //    Console.WriteLine("Press any key to disarm; you have 5 seconds.");
+            //    bool canceled = token.WaitHandle.WaitOne(5000);
+            //    Console.WriteLine(canceled ? "Bomb disarm" : "BOOM!");
+            //}, token);
+            //t.Start();
+
+            //Console.ReadLine();
+            //cts.Cancel();
+
+            #endregion
+
+            #region Waiting for Tasks
+            //var cts = new CancellationTokenSource();
+            //var token = cts.Token;
+
+            //var t = new Task(() =>
+            //{
+            //    Console.WriteLine("I take 5 seconds.");
+            //    for (int i = 0; i < 5; i++)
+            //    {
+            //        token.ThrowIfCancellationRequested();
+            //        Thread.Sleep(1000);
+            //    }
+
+            //    Console.WriteLine("I am done.");
+            //}, token);
+            //t.Start();
+
+            //Task t2 = Task.Factory.StartNew(() =>
+            //{
+            //    Thread.Sleep(3000);
+            //}, token);
+
+            //Task.WaitAll(t, t2);
+
+            //Console.WriteLine($"Task t status is {t.Status}");
+            //Console.WriteLine($"Task t2 status is {t2.Status}");
+
+            #endregion
+
+            #region Exception Handling
+
+            //An unobserved task exception will not get handled, unless task.Wait() will catch the exceptions
+            //var t = Task.Factory.StartNew(() => throw new InvalidOperationException() { Source = "t" });
+            //var t2 = Task.Factory.StartNew(() => throw new AccessViolationException() { Source = "t2" });
+
+            //try
+            //{
+            //    Task.WaitAll(t, t2);
+            //}
+            //catch (AggregateException ae)
+            //{
+            //    foreach (var e in ae.InnerExceptions)
+            //    {
+            //        Console.WriteLine($"Exception {e.GetType()} from {e.Source}");
+            //    }
+            //}
+
+            #endregion
+
+            Console.WriteLine("Main program done.");
+            Console.ReadLine();
+        }
+    }
+}
